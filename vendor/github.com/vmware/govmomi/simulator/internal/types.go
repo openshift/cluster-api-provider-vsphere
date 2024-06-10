b014a333553e63ba037f4a5269ff4a71b25f75c9
Damiano Donati <damiano.donati@gmail.com>
2024-06-10 16:11:05 +0200

    UPSTREAM: <carry>: Updating and vendoring go modules after an upstream rebase

--- vendor/github.com/vmware/govmomi/simulator/internal/types.go
+++ vendor/github.com/vmware/govmomi/simulator/internal/types.go
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2019 VMware, Inc. All Rights Reserved.
+Copyright (c) 2014-2022 VMware, Inc. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -41,6 +41,7 @@ type FetchResponse struct {
 }
 
 type FetchBody struct {
+	Req    *Fetch         `xml:"Fetch,omitempty"`
 	Res    *FetchResponse `xml:"FetchResponse,omitempty"`
 	Fault_ *soap.Fault    `xml:"http://schemas.xmlsoap.org/soap/envelope/ Fault,omitempty"`
 }
